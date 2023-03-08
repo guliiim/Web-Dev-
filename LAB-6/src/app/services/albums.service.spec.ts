faf36b6cd09dfd2f0671468868d1bd6e7fd85387
guliiim <118511119+guliiim@users.noreply.github.com>
2023-03-08 20:14:52 +0600

    Add files via upload

---
+++ LAB-6/src/app/services/albums.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { AlbumsService } from './albums.service';
+
+describe('AlbumsService', () => {
+  let service: AlbumsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AlbumsService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
